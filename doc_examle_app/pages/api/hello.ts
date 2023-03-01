ad5d0a8605dcae0cd0d712ab59be03d189e9ff9a
qazplm807733@gmail.com <qazplm807733@gmail.com>
2023-03-01 21:13:55 +0900

    nextjs + postgre with yml

--- doc_examle_app/pages/api/hello.ts
+++ doc_examle_app/pages/api/hello.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+    },
+  });
+
+  const allUsers = await prisma.user.findMany();
+
+  res.status(200).json(allUsers as any);
 }
